4f4c5ef56a3a0029ec47f82b92eed9ce2630cec2
Luvwahraan <luvwahraan@pikaed.org>
2017-02-19 21:30:13 +0100

    detecting fat fs

--- backup.sh
+++ backup.sh
@@ -1,28 +1,14 @@
 #!/usr/bin/env bash
 
-BAK_DIR="$1"
+BAK_DIR=''
 USE_ZENITY=0
-LIMIT_SIZE=768
-
 BAK_GPG=0
 BAK_GK=0
 BAK_CFG=0
-DO_NOTHING=0
-
-RSYNC_CMD='rsync -a --no-o --no-p --no-g --safe-links --modify-window 1 --del \
-  --stats --ignore-errors'
+LIMIT_SIZE=768 # 768MB
+SILENT=0
 
 
-
-function say
-{
-  if [ $USE_ZENITY == 1 ] ; then
-    zenity --info --text "`echo $@ | sed 's/^\n//'`"
-  else
-   #MSG = "`echo $@ | sed 's/^\n//'`"
-    echo -e $@  | fold -s
-  fi
-}
 
 
 function usage
@@ -38,15 +24,44 @@ Everyone can reads thems, including _private keys_!\n
 Use external data storage like USB flash drive, and remove it after backup.\n
 \n
 OPTIONS:\n
-   -h      Show this message\n
-   -z      Use zenity instead echo on terminal\n
-   -g      Backup all GPG keys\n
-   -k      Backup gnome keyring\n
-   -s      Ignore directories greater, in Mio ; default 768 (0,75 Gio)\n
-   -d      Choose directory where write backup ; required\n"
+   -a     all\n
+          Active all backup options\n
+   -c     config\n
+          Backup ~/.config\n
+   -d     directory\n
+          Choose directory where write backup ; required\n
+   -g     gpg\n
+          Backup all GPG keys\n
+   -h     help\n
+          Show this message\n
+   -k     keyring\n
+          Backup gnome keyring\n
+   -m     max size\n
+          Ignore directories greater; default 768 (0,75 GB)\n
+   -s     silent\n
+          No output except errors\n
+   -z     zenity\n
+          Use zenity instead echo on terminal\n"
 
   say $MSG
   exit
+}
+
+function fs_type
+{
+  echo `df --output=fstype $@ | tail -n1`
+}
+
+function say
+{
+  if [ ! "$SILENT" == 1 ] ; then
+    if [ $USE_ZENITY == 1 ] ; then
+      zenity --info --text "`echo $@ | sed 's/^\n//'`"
+    else
+     #MSG = "`echo $@ | sed 's/^\n//'`"
+      echo -e $@  # | fold -s
+    fi
+  fi
 }
 
 function bak_gpg
@@ -59,15 +74,19 @@ function bak_gpg
 
 function bak_gk
 {
-  $RSYNC_CMD "/home/$USER/.local/share/keyrings/" "$BAK_DIR"
+  $RSYNC_CMD "/home/$USER/.local/share/keyrings/" "$BAK_DIR/keyrings"
 }
 
 function bak_cfg
 {
+  DIR="/home/$USER/.config"
+  pushd $DIR > /dev/null
+  LIMIT_SIZE=$((1024*1024*$LIMIT_SIZE))
+
   # Get file too big
-  EXCLUDES=''
+  EXCLUDES='--exclude=cache* '
   PRINTABLE_EXCLUDES=''
-  for f in `du --threshold $LIMIT_SIZE * | cut -f2` ; do
+  for f in `du --threshold $LIMIT_SIZE -s * | cut -f2` ; do
     EXCLUDES="$EXCLUDES--exclude=$f "
     PRINTABLE_EXCLUDES="$PRINTABLE_EXCLUDES$f "
   done
@@ -80,13 +99,19 @@ function bak_cfg
     MSG="$MSG\n\nDirectories excluded (over $LIMIT_SIZE Mio) :\n$PRINTABLE_EXCLUDES"
   fi
   say $MSG
+  popd > /dev/null
 }
 
 
-while getopts “cd:ghkns:z” OPTION ; do
+while getopts “cad:ghknm:z” OPTION ; do
 case $OPTION in
   z)
     USE_ZENITY=1
+    ;;
+  a)
+    BAK_CFG=1
+    BAK_GK=1
+    BAK_GPG=1
     ;;
   c)
     BAK_CFG=1
@@ -103,11 +128,11 @@ case $OPTION in
   k)
     BAK_GK=1
     ;;
-  n)
-    DO_NOTHING=1
+  m)
+    LIMIT_SIZE=$OPTARG
     ;;
   s)
-    LIMIT_SIZE=$OPTARG
+    SILENT=1
     ;;
   ?)
     usage
@@ -116,22 +141,35 @@ esac
 done
 
 if [ -z "$BAK_DIR" ] ; then
+  say "Where have I to write backups?\n"
   usage
 fi
 
 if [ ! -d "$BAK_DIR" ] ; then
-  echo "Unable to read $BAK_DIR"
+  say "Unable to read $BAK_DIR\n"
   exit 1
 fi
 
 
-NOW=`date +%F`
+# FAT fs doesn't support links and permissions
+RSYNC_OPTIONS='-a --safe-links --del --ignore-errors'
+if [ -n "`fs_type $BAK_DIR | sed -n '/fat/ {p}'`" ] ; then
+  RSYNC_OPTIONS="$RSYNC_OPTIONS --no-o --no-p --no-g --modify-window 1 "
+fi
+
+
+RSYNC_CMD="rsync $RSYNC_OPTIONS "
 BAK_DIR="$BAK_DIR/backup"
-DIR="/home/$USER/.config"
-LIMIT_SIZE=$((1024*1024*$LIMIT_SIZE)) # 1Gio
+NOW=`date +%F`
+
 
 say "Dont't forget: use external data storage like USB flash drive, and remove it after backup.\n"
 if [ "$DO_NOTHING" == 1 ] ; then exit ; fi
+
+if [ "$BAK_GPG" == 0 ] && [ "$BAK_GK" == 0 ] && [ "$BAK_CFG" == 0 ] ; then
+  say "Nothing to backup.\n"
+  usage
+fi
 
 
 # Backup gpg
